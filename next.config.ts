7e625c4b513adcf1994e478807d7d68d0ac67dc6
Seu Nome Completo <seu.email@exemplo.com>
2026-09-01 14:49:20 -0300

    Injetar variaveis de ambiente diretamente no next.config.ts para o build do Cloudflare

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: "https://wldifxcwobyeqbvwatgr.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsZGlmeGN3b2J5ZXFidndhdGdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyNjczMjIsImV4cCI6MjEwMzg0MzMyMn0.RG6CnZP9TRAdclSXp0FeixhbakR3Dq-vittH427X9ZM"
+  },
   allowedDevOrigins: ['172.16.0.24'],
 };
 
